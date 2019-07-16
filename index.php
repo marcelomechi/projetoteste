c7cef42c476cd01fbe185690e3eb6cb2964c3185
Marcelo <Marcelo@Marcelo-PC>
2019-07-16 20:32:14 -0300

    Esse é meu segundo commit.

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
  4 clicamos com o botao direito no projeto, em seguida em GIT e iniciar repositorio GIT, ai sim ele estará associado ao projeto do github
  5 para trazer os arquivos que já estão no repositorio, clicamos com o botão direito em GIT / REMOTO /  PUXAR (caso não funcione antes de dar o push
    basta dar um pull antes)
+6 para salvar as alterações basta ir em GIT e dar um commit todos vão ficar pretos
+  so tem um pequeno detalhe, para ele aparecer no repositorio (www.github.com) você tem que dar um push
+  para ele confirmar tudo e enviar para o repositorio dando um push
 
 
 Todos os arquivos que ficam em verde eles estão na maquina mas não foram enviados ao repositório
