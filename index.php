5c04d68bde654253759bb101ce3414e1c4e8beb4
Marcelo <Marcelo@Marcelo-PC>
2019-07-16 20:29:10 -0300

    Esse é meu segundo commit.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+ 1 vamos no menu TEAM / GIT, colamos a URL do repositório, digitamos nossos dados de acesso.
+ 2 selecionamos a ramificação
+ 3 ele vai perguntar se deseja criar um projeto usando a url clonada
+ 4 clicamos com o botao direito no projeto, em seguida em GIT e iniciar repositorio GIT, ai sim ele estará associado ao projeto do github
+ 5 para trazer os arquivos que já estão no repositorio, clicamos com o botão direito em GIT / REMOTO /  PUXAR (caso não funcione antes de dar o push
+   basta dar um pull antes)
+
+
+Todos os arquivos que ficam em verde eles estão na maquina mas não foram enviados ao repositório
+
+se ficar azul é porque houve modificação
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
